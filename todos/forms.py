a4dd5296ea4a2316a73f8c7dd1c67c8731b9cb2a
Ogimatsu <ogimattsu@gmail.com>
2025-05-03 14:55:18 +0900

    feat: 検索機能の追加

--- todos/forms.py
+++ todos/forms.py
@@ -1,17 +1,74 @@
 from django import forms
 from .models import Todo
 
-#Todoモデルのフォーム
+# 新規作成・編集用フォーム
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = '__all__'
+        widgets = {
+            # 締切日時は日付の入力欄にする
+            'due_date': forms.DateInput(attrs={
+                                    'type': 'date',
+                                    'max': '9999-12-31',
+                                    'class': 'form-control'})
+            }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
+        for field_name, field in self.fields.items():
             # フィールドの種類によってクラスをつける（テキストかセレクトかで）
-            if isinstance(field.widget, forms.widgets.TextInput) or isinstance(field.widget, forms.widgets.Textarea):
+            if field_name == 'due_date':
+                continue
+            elif isinstance(field.widget, forms.widgets.TextInput) or isinstance(field.widget, forms.widgets.Textarea):
                 field.widget.attrs['class'] = 'form-control'
             elif isinstance(field.widget, forms.widgets.Select):
                 field.widget.attrs['class'] = 'form-select'
+
+# 選択フォーム取得用
+def get_choice_form(choice_menu,choice_label):
+    return forms.ChoiceField(required=False,
+                        choices=choice_menu,
+                        label=choice_label,
+                        widget=forms.Select(attrs={'class':'form-select'})
+                    )
+
+# 年が4桁以内かを確認する
+def validate_year_within_limit(value):
+    if value and value.year > 9999:
+        raise forms.ValidationError("年は4桁以内で入力してください。")
+
+# 日時フォーム取得用
+def get_date_form(date_label):
+    return forms.DateField(required=False,
+                        widget=forms.DateInput(attrs={
+                            'type': 'date',
+                            'max': '9999-12-31',
+                            'class': 'form-control'
+                        }),
+                        label=date_label,
+                        validators=[validate_year_within_limit]
+                    )
+
+# ソート検索用フォーム
+class TodoSearchForm(forms.Form):
+    # タスク名
+    title = forms.CharField(required=False, label='タスク名', widget=forms.TextInput(attrs={'class':'form-control'}))
+    # 緊急度
+    urgency = get_choice_form([('', '-----'), (0, '低'), (1, '高')],'緊急度')
+    # 重要度
+    importance = get_choice_form([('', '-----'), (0, '低'), (1, '高')], '重要度')
+    # タスク状態
+    status = get_choice_form([('', '-----'), (0, '未完了'), (1, '完了')], 'タスク状態')
+    # 更新日時
+    updated_from = get_date_form('更新日時（開始）')
+    updated_to = get_date_form('更新日時（終了）')
+    # 作成日時
+    created_from = get_date_form('作成日時（開始）')
+    created_to = get_date_form('作成日時（終了）')
+    # 締切日時
+    due_date_from = get_date_form('締切日時（開始）')
+    due_date_to = get_date_form('締切日時（終了）')
+    # 完了日時
+    completed_from = get_date_form('完了日時（開始）')
+    completed_to = get_date_form('完了日時（終了）')
--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,63 @@
+from datetime import datetime, time
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.urls import reverse
 from todos.models import Todo
-from todos.forms import TodoForm
+from todos.forms import TodoForm, TodoSearchForm
+
+# 各種日付の範囲検索
+def get_search_date(queryset,field,date_start,date_end):
+    # 開始日が入力されている場合、開始日は入力した日の00:00:00を設定
+    if date_start:
+        date_start = datetime.combine(date_start,time.min)
+    # 終了日が入力されている場合、終了日は入力した日の23:59:59を設定
+    if date_end:
+        date_end = datetime.combine(date_end,time.max)
+
+    # 開始日及び終了日をもとに範囲検索を行う
+    if date_start and date_end and (date_start <= date_end):
+        return queryset.filter(**{f"{field}__range":(date_start, date_end)})
+    elif date_start:
+        return queryset.filter(**{f"{field}__gte":(date_start)})
+    elif date_end:
+        return queryset.filter(**{f"{field}__lte":(date_end)})
+    return queryset
 
 def index(request):
+    action = request.GET.get('action')
+    form = TodoSearchForm((request.GET or None) if action == 'search' else None)
     todos = Todo.objects.all()
-    return render(request, 'todos/index.html', {'todos': todos})
+
+    # 検索ボタン押下時、検索を行う
+    if action == 'search':
+        if form.is_valid():
+            cd = form.cleaned_data
+            # タスク名を入れている場合、部分検索を行う
+            if cd['title']:
+                todos = todos.filter(title__icontains=cd['title'])
+            # 緊急度を入力している場合、条件検索を行う
+            if cd['urgency']:
+                todos = todos.filter(urgency=cd['urgency'])
+            # 重要度を入力している場合、条件検索を行う
+            if cd['importance']:
+                todos = todos.filter(importance=cd['importance'])
+            # ステータスを入力している場合、条件検索を行う
+            if cd['status']:
+                todos = todos.filter(status=cd['status'])
+            # 更新日時を入力している場合、範囲検索を行う
+            todos = get_search_date(todos,'updated_at',cd['updated_from'],cd['updated_to'])
+            # 作成日時を入力している場合、範囲検索を行う
+            todos = get_search_date(todos,'created_at',cd['created_from'],cd['created_to'])
+            # 締切日時を入力している場合、範囲検索を行う
+            todos = get_search_date(todos,'due_date',cd['due_date_from'],cd['due_date_to'])
+            # 完了日時を入力している場合、範囲検索を行う
+            todos = get_search_date(todos,'completed_at',cd['completed_from'],cd['completed_to'])
+
+    # 更新日時の降順に並び替え
+    todos = todos.order_by('-updated_at')
+    # 検索結果の件数を取得
+    count = todos.count()
+    return render(request, 'todos/index.html', {'todos': todos, 'count': count, 'form': form})
 
 def create(request):
     if request.method == 'POST':
