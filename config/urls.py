ba8e9e622a155ffd5f048c6d3ace305132c1050a
Ogimatsu <ogimattsu@gmail.com>
2025-06-06 00:20:33 +0900

    fix:ログインしているのにログイン画面になるのを修正

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from todos.views import index
 
 urlpatterns = [
-    path('',include('users.urls')),
+    path('', index, name='index'),
     path('todos/', include('todos.urls', namespace='todos')),
     path('admin/', admin.site.urls),
+    path('', include('users.urls')),
 ]
